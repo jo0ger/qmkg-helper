1478b255e37eadab40b6c9246297bfae61971740
Jooger <zzy1198258955@163.com>
2018-02-17 22:13:38 +0800

    [update] update pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+/**
+ * @desc PM2 deploy
+ * @author Jooger <zzy1198258955@163.com>
+ * @date 17 Feb 2018
+ */
+
+const packageInfo = require('./package.json')
+
+module.exports = {
+  deploy: {
+    production: {
+      user: 'root',
+      host: 'jooger.me',
+      ref: 'origin/master',
+      repo: packageInfo.repository.url,
+      path: '/root/www/' + packageInfo.name,
+      'post-deploy': 'git pull && cnpm install && pm2 stop all && npm run build && pm2 start all'
+    }
+  }
+}
